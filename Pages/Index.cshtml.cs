f962b13c454e620541f955fae6826343365203e4
Егор Белов <e1belov@yandex.ru>
2023-06-25 16:17:56 +0500

    111

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,6 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ServiceReference1;
-
 
 namespace lab7.Pages
 {
@@ -11,15 +9,22 @@ namespace lab7.Pages
         [BindProperty]
         public double Fahrenheit { get; set; }
 
-        public double? Result { get; set; }
+        [BindProperty]
+        public double Celsius { get; set; }
 
-        public async Task OnPostAsync()
+        public double? CelsiusResult { get; set; }
+        public double? FahrenheitResult { get; set; }
+
+        public async Task OnPostToFahrenheitAsync()
         {
-            // Создаем прокси-клиента для вашего WCF-сервиса
             var client = new ConversionServiceClient();
+            FahrenheitResult = await client.CelsiusToFahrenheitAsync(Celsius);
+        }
 
-            // Вызываем метод сервиса для конвертации фаренгейтов в цельсии
-            Result = await client.FahrenheitToCelsiusAsync(Fahrenheit);
+        public async Task OnPostToCelsiusAsync()
+        {
+            var client = new ConversionServiceClient();
+            CelsiusResult = await client.FahrenheitToCelsiusAsync(Fahrenheit);
         }
     }
 }
